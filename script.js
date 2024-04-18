94483d727e0160326a51973068731e36e77247e5
arslaan07 <arslaanbanday5@gmail.com>
2024-04-19 02:25:55 +0530

    countdownTill2025

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+const targetDate = new Date('2025-01-01T00:00:00');
+function updateCountDown() {
+    const currentDate = new Date();
+    const timeDifference = targetDate - currentDate;
+
+    const days = Math.floor(timeDifference / (1000*60*60*24));
+    const hours = Math.floor((timeDifference % (1000*60*60*24)) / (1000*60*60));
+    const minutes = Math.floor((timeDifference % (1000*60*60)) / (1000*60));
+    const seconds = Math.floor((timeDifference % (1000*60)) / (1000));
+    let cd = document.getElementById('countdown');
+    cd.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    setTimeout(updateCountDown, 1000);
+
+}
+
+
+document.getElementById('countdown-btn').addEventListener("click", function() {
+    this.remove();
+    updateCountDown();
+});
+
+});
